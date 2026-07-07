e7486b809cd9d47d848f8e7903361914c617e09d
Jorgee-hub <jmontiek217@gmail.com>
2026-07-07 02:41:44 +0000

    feat: add dropdown button for favorites

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
-
+	const { store, dispatch } = useGlobalReducer();
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
@@ -9,11 +10,32 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">Star wars</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Favorites</button>
-					</Link>
+					<div className="dropdown">
+						<button
+							className="btn btn-secondary dropdown-toggle"
+							type="button"
+							data-bs-toggle="dropdown"
+							aria-expanded="false"
+						>
+							Favorites {store.favorites.length}
+						</button>
+						<ul className="dropdown-menu">
+							{store.favorites.map((favorite, favoriteIndex) => {
+								return (
+									<li key={`${favorite.category}-${favorite.uid}`}>{favorite.name}
+										<i onClick={(e) => {
+											e.stopPropagation();
+											dispatch({type: "remove_favorite", payload: favorite })
+										}} className = "fa-solid fa-trash" ></i>
+									
+									</li>
+						)
+							})}
+					</ul>
 				</div>
+
 			</div>
-		</nav>
+		</div>
+		</nav >
 	);
 };
