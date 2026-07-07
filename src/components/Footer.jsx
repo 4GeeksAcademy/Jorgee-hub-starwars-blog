7fdcb526c6c0d4a3cb6f8273cbeeddae85f73cdd
Jorgee-hub <jmontiek217@gmail.com>
2026-07-07 00:36:17 +0000

    style: add footer copyrights

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			footer
+		<p className="text-secondary">
+			© 2026 Jorgee-hub. Todos los derechos reservados.
 		</p>
 	</footer>
 );
